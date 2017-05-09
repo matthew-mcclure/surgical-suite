d563373ba27beec8ddf65726d18f02d53fb44e51
matthew mcclure <mcclure0728@gmail.com>
2017-05-08 22:01:07 -0700

    heroku t5

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080
 mongoose.Promise = global.Promise
 
 
-//mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017')
+mongoose.connect(mongodb://admin:adminpassword@ds123381.mlab.com:23381/mean-template || 'mongodb://localhost:27017')
 
 process.on('SIGINT', function() {  
   mongoose.connection.close(function () { 
